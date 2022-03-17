5c14a5836c13f109c70afa402abe96c5bf03a7a1
Valerii Bihun <valeriibihun.co@gmail.com>
2022-03-16 20:54:01 -0600

    rendering fight results

--- pokedex-app/src/pokedex/Pokedex.jsx
+++ pokedex-app/src/pokedex/Pokedex.jsx
@@ -4,52 +4,101 @@ import "./Pokedex.css";
 import { useState } from "react";
 
 const Pokedex = () => {
-
   const [experienceTeamOne, setExperienceTeamOne] = useState(0);
   const [experienceTeamTwo, setExperienceTeamTwo] = useState(0);
-  
+  const [draw, setDraw] = useState(false);
+  const [teamOneWin, setTeamOneWin] = useState(false);
+  const [teamTeoWin, setTeamTwoWin] = useState(false);
+
+  const calculatePower = (e) => {
+    e.preventDefault();
+    document.getElementById("btnTeam1").click()
+    document.getElementById("btnTeam2").click()
+    if(experienceTeamOne == experienceTeamTwo){
+      setDraw(true)
+    };
+    if(experienceTeamOne > experienceTeamTwo){
+      setTeamOneWin(true);
+    }
+    if(experienceTeamTwo > experienceTeamOne){
+      setTeamTwoWin(true);
+    }
+  }
+
+
   const calculateExpTeam1 = (e) => {
-    let mainElement = e.currentTarget.previousElementSibling.children[0].children[0].getAttribute("data-id")
-    console.log(mainElement)
-    let previousElement = e.currentTarget.previousElementSibling
-    let exp2 = previousElement.previousElementSibling.children[0].children[0].getAttribute("data-id")
-    console.log(exp2)
-    let previousElement2 = previousElement.previousElementSibling
-    let exp3 = previousElement2.previousElementSibling.children[0].children[0].getAttribute("data-id")
-    console.log(exp3)
-    let previousElement3 = previousElement2.previousElementSibling
-    let exp4 = previousElement3.previousElementSibling.children[0].children[0].getAttribute("data-id")
-    console.log(exp4)
-    let totalExp = Number(mainElement) + Number(exp2) + Number(exp3) + Number(exp4)
-    setExperienceTeamOne(totalExp)
-    console.log(experienceTeamOne)
+    e.preventDefault();
+    let mainElement =
+      e.currentTarget.previousElementSibling.children[0].children[0].getAttribute(
+        "data-id"
+      );
+
+    let previousElement = e.currentTarget.previousElementSibling;
+    let exp2 =
+      previousElement.previousElementSibling.children[0].children[0].getAttribute(
+        "data-id"
+      );
+
+    let previousElement2 = previousElement.previousElementSibling;
+    let exp3 =
+      previousElement2.previousElementSibling.children[0].children[0].getAttribute(
+        "data-id"
+      );
+
+    let previousElement3 = previousElement2.previousElementSibling;
+    let exp4 =
+      previousElement3.previousElementSibling.children[0].children[0].getAttribute(
+        "data-id"
+      );
+
+    let totalExp =
+      Number(mainElement) + Number(exp2) + Number(exp3) + Number(exp4);
+    setExperienceTeamOne(totalExp);
+    console.log("Team 1 total EXP: " + experienceTeamOne);
     // setExperienceTeamOne(mainElement + exp2 + exp3 + exp4)
     // console.log("This is EXP sum: " + experienceTeamOne)
-  }
+  };
 
- const calculateExpTeam2 = (e) => {
-    let mainElement = e.currentTarget.previousElementSibling.children[0].children[0].getAttribute("data-id")
-    console.log(mainElement)
-    let previousElement = e.currentTarget.previousElementSibling
-    let exp2 = previousElement.previousElementSibling.children[0].children[0].getAttribute("data-id")
-    console.log(exp2)
-    let previousElement2 = previousElement.previousElementSibling
-    let exp3 = previousElement2.previousElementSibling.children[0].children[0].getAttribute("data-id")
-    console.log(exp3)
-    let previousElement3 = previousElement2.previousElementSibling
-    let exp4 = previousElement3.previousElementSibling.children[0].children[0].getAttribute("data-id")
-    console.log(exp4)
-    let totalExp = Number(mainElement) + Number(exp2) + Number(exp3) + Number(exp4)
-    setExperienceTeamTwo(totalExp)
-    console.log(experienceTeamTwo)
+  const calculateExpTeam2 = (e) => {
+    e.preventDefault();
+    let mainElement =
+      e.currentTarget.previousElementSibling.children[0].children[0].getAttribute(
+        "data-id"
+      );
+
+    let previousElement = e.currentTarget.previousElementSibling;
+    let exp2 =
+      previousElement.previousElementSibling.children[0].children[0].getAttribute(
+        "data-id"
+      );
+
+    let previousElement2 = previousElement.previousElementSibling;
+    let exp3 =
+      previousElement2.previousElementSibling.children[0].children[0].getAttribute(
+        "data-id"
+      );
+
+    let previousElement3 = previousElement2.previousElementSibling;
+    let exp4 =
+      previousElement3.previousElementSibling.children[0].children[0].getAttribute(
+        "data-id"
+      );
+
+    let totalExp =
+      Number(mainElement) + Number(exp2) + Number(exp3) + Number(exp4);
+    setExp2(totalExp);
+    console.log("Team 2 total EXP: " + experienceTeamTwo);
     // setExperienceTeamOne(mainElement + exp2 + exp3 + exp4)
     // console.log("This is EXP sum: " + experienceTeamOne)
-  }
-  
+  };
 
+  const setExp2 = (totalExp) => {
+    setExperienceTeamTwo(totalExp);
+  };
   return (
     <div>
       <div>
+        <p>Hi</p>
         <div className="Pokedex">
           <div className="column">
             <Pokecard />
@@ -63,10 +112,13 @@ const Pokedex = () => {
           <div className="column">
             <Pokecard />
           </div>
-          <button id="btn" onClick={calculateExpTeam1}>Calculate Exp</button>
+          <button id="btnTeam1" onClick={(e) => calculateExpTeam1(e)}>
+            Calculate Exp
+          </button>
         </div>
       </div>
       <div>
+      <p>Hi</p>
         <div className="Pokedex">
           <div className="column">
             <Pokecard />
@@ -80,8 +132,15 @@ const Pokedex = () => {
           <div className="column">
             <Pokecard />
           </div>
-          <button id="btn" onClick={calculateExpTeam2}>Calculate Exp</button>
+          <button id="btnTeam2" onClick={(e) => calculateExpTeam2(e)}>
+            Calculate Exp
+          </button>
         </div>
+      </div>
+      <div>
+      <button id="btnTeam3" onClick={(e) => calculatePower(e)}>
+            Calculate Power and Fight!
+          </button>
       </div>
     </div>
   );
