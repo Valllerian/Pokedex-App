5dfeb17742d371a62d6520debf58950158c9c5a3
Valerii Bihun <valeriibihun.co@gmail.com>
2022-03-17 23:45:01 -0600

    Code formatted and prettyfied

--- pokedex-app/src/pokecard/Pokecard.jsx
+++ pokedex-app/src/pokecard/Pokecard.jsx
@@ -1,41 +1,41 @@
-import React from 'react'
-import './Pokecard.css'
+import React from "react";
+import "./Pokecard.css";
 import { useState, useEffect } from "react";
 
 const Pokecard = (props) => {
-  
-  const [exp, setExp] = useState(0)
+  const [exp, setExp] = useState(0);
 
-  const  pokemons = [
-    {id: 4, name: 'Charmander', type: 'fire', base_experience: 62},
-    {id: 7, name: 'Squitle', type: 'water', base_experience: 63},
-    {id: 11, name: 'Metapod', type: 'bug', base_experience: 72},
-    {id: 12, name: 'Buttlefree', type: 'flying', base_experience: 178},
-    {id: 25, name: 'Pikachu', type: 'electric', base_experience: 112},
-    {id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95},
-    {id: 94, name: 'Gengar', type: 'poison', base_experience: 225},
-    {id: 133, name: 'Eevee', type: 'normal', base_experience: 65},
-  ]
+  const pokemons = [
+    { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
+    { id: 7, name: "Squitle", type: "water", base_experience: 63 },
+    { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
+    { id: 12, name: "Buttlefree", type: "flying", base_experience: 178 },
+    { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
+    { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
+    { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
+    { id: 133, name: "Eevee", type: "normal", base_experience: 65 },
+  ];
 
-  let randomiser = pokemons[Math.floor(Math.random()*pokemons.length)];
+  let randomiser = pokemons[Math.floor(Math.random() * pokemons.length)];
 
-useEffect(() => {
-  props.setExp(randomiser.base_experience)
-}, [])
+  useEffect(() => {
+    props.setExp(randomiser.base_experience);
+  }, []);
 
-
-  
   return (
-      <div>
+    <div>
       <div data-id={randomiser.base_experience}>
-          <p>{randomiser.name}</p>
-          <img className="Pokemon-img" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randomiser.id}.png`} alt="pokemon image"/>
-          <p>Type: {randomiser.type}</p>
-          <p>EXP: {randomiser.base_experience}</p>
-        </div>
+        <p>{randomiser.name}</p>
+        <img
+          className="Pokemon-img"
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randomiser.id}.png`}
+          alt="pokemon image"
+        />
+        <p>Type: {randomiser.type}</p>
+        <p>EXP: {randomiser.base_experience}</p>
+      </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Pokecard
+export default Pokecard;
--- pokedex-app/src/pokedex/Pokedex.jsx
+++ pokedex-app/src/pokedex/Pokedex.jsx
@@ -4,37 +4,32 @@ import "./Pokedex.css";
 import { useState, useEffect } from "react";
 
 const Pokedex = (props) => {
+  const [expCard1, setExpCard1] = useState(0);
+  const [expCard2, setExpCard2] = useState(0);
+  const [expCard3, setExpCard3] = useState(0);
+  const [expCard4, setExpCard4] = useState(0);
 
-const [expCard1, setExpCard1] = useState(0)
-const [expCard2, setExpCard2] = useState(0)
-const [expCard3, setExpCard3] = useState(0)
-const [expCard4, setExpCard4] = useState(0)
+  const [test, setTest] = useState(0);
 
-const[totalExp, setTotalExp] = useState(0)
-
-const [test, setTest] = useState(0)
-
-useEffect(() => {
-  // setTotalExp(expCard1 + expCard2 + expCard3 + expCard4)
-  props.setTest(expCard1 + expCard2 + expCard3 + expCard4)
-}, [expCard1, expCard2, expCard3, expCard4])
-
+  useEffect(() => {
+    props.setTest(expCard1 + expCard2 + expCard3 + expCard4);
+  }, [expCard1, expCard2, expCard3, expCard4]);
 
   return (
     <div>
       <div>
         <div className="Pokedex">
           <div className="column">
-            <Pokecard setExp={exp => setExpCard1(exp)} />
+            <Pokecard setExp={(exp) => setExpCard1(exp)} />
           </div>
           <div className="column">
-            <Pokecard setExp={exp => setExpCard2(exp)} />
+            <Pokecard setExp={(exp) => setExpCard2(exp)} />
           </div>
           <div className="column">
-            <Pokecard setExp={exp => setExpCard3(exp)} />
+            <Pokecard setExp={(exp) => setExpCard3(exp)} />
           </div>
           <div className="column">
-            <Pokecard setExp={exp => setExpCard4(exp)} />
+            <Pokecard setExp={(exp) => setExpCard4(exp)} />
           </div>
         </div>
       </div>
